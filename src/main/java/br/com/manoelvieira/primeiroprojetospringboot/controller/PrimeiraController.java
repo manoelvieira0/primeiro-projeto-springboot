0622fe2f6ece2c435e661a36f0233de68d4af4a3
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-06 11:19:42 -0300

    Método GET

--- src/main/java/br/com/manoelvieira/primeiroprojetospringboot/controller/PrimeiraController.java
+++ src/main/java/br/com/manoelvieira/primeiroprojetospringboot/controller/PrimeiraController.java
@@ -1,4 +1,4 @@
-package br.com.manoelvieira.primeiroprojetospringboot;
+package br.com.manoelvieira.primeiroprojetospringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
