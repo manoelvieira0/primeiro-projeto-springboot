865937e896b08b824352f6c9bf046a7338858184
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-06 11:25:20 -0300

    Path Params

--- src/main/java/br/com/manoelvieira/primeiroprojetospringboot/controller/PrimeiraController.java
+++ src/main/java/br/com/manoelvieira/primeiroprojetospringboot/controller/PrimeiraController.java
@@ -1,16 +1,30 @@
 package br.com.manoelvieira.primeiroprojetospringboot.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiracontroller")
 public class PrimeiraController {
 
-  @GetMapping("/primeirometodo")
-  public String primeiroMetodo(){
-    return "Primeiro metodo";
+  @GetMapping("/primeirometodo/{id}")
+  public String primeiroMetodo(@PathVariable String id) {
+    return "O Parâmetro é " + id;
   }
-  
+
+  @GetMapping("/metodocomqueryparams")
+  public String metodoComQueryParams(@RequestParam String id) {
+    return "O Parâmetro com metodocomqueryparams é " + id;
+  }
+
+  @GetMapping("/metodocomqueryparams2")
+  public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+    return "O Parâmetro com metodocomqueryparams é " + allParams.entrySet();
+  }
+
 }
